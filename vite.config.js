bd3510abb2905568f4565e7a1c5c8ebe0e2104e9
Prograperor <104015031+BENELMALIH-Ahmed-Amine@users.noreply.github.com>
2025-06-28 20:24:33 +0100

    Update vite.config.js
    correct

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  dase: "HowksCute-Anime-react-API"
+  base: "HowksCute-Anime-react-API"
 })
